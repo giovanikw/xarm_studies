af9d090c0911f38e6fd2dc61c624d4a4f6a38ca1
giovanikw <giovanikwi@gmail.com>
2026-03-12 08:04:26 -0300

    Fixed msg type in arm declaration

--- First_CPP/src/First_CPP_nd.cpp
+++ First_CPP/src/First_CPP_nd.cpp
@@ -109,45 +109,15 @@ class ArmPublisher
     ArmPublisher()
     : Node("arm_publisher")
     {
-      using moveit::planning_interface::MoveGroupInterface;
-
-      auto const node = std::make_shared<rclcpp::Node>(
-      "hello_moveit",
-      rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true)
-    );
+    //   arm_pub_= this->create_publisher<moveit_msgs::msg::Pose>("armPA", 10);
     }
 
   private:
     void MoveArm()
     {
-      // Create the MoveIt MoveGroup Interface
-auto move_group_interface = MoveGroupInterface(node, "manipulator");
-
-// Set a target Pose
-auto const target_pose = []{
-  geometry_msgs::msg::Pose msg;
-  msg.orientation.w = 1.0;
-  msg.position.x = 0.28;
-  msg.position.y = -0.2;
-  msg.position.z = 0.5;
-  return msg;
-}();
-move_group_interface.setPoseTarget(target_pose);
-
-// Create a plan to that target pose
-auto const [success, plan] = [&move_group_interface]{
-  moveit::planning_interface::MoveGroupInterface::Plan msg;
-  auto const ok = static_cast<bool>(move_group_interface.plan(msg));
-  return std::make_pair(ok, msg);
-}();
-
-// Execute the plan
-if(success) {
-  move_group_interface.execute(plan);
-} else {
-  ;
-}
+      ;
     };
+    rclcpp::Publisher<moveit_msgs::msg::Grasp_>::SharedPtr arm_pub_;
 };
 
 
